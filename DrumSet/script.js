a01565e1e1ac250706aff1ce37e7f631dcadf853
Akash Parmar <ak.parmar1442000@gmail.com>
2022-09-18 03:01:54 +0530

    OneDayJsProject

---
+++ DrumSet/script.js
@@ -0,0 +1,21 @@
+const keys = document.querySelectorAll(".keys");
+
+
+function removeTransition(e){
+    this.classList.remove('playing');
+}
+
+keys.forEach(key=>key.addEventListener('transitionend',removeTransition));
+
+
+
+window.addEventListener('keydown',(e)=>{
+    const audio = document.querySelector(`audio[data-val="${e.keyCode}"]`);
+    const key = document.querySelector(`.keys[data-val="${e.keyCode}"]`);
+    if(!audio) return; // stops function running alltogether
+    audio.currentTime = 0; // if we press audio again & again we want it to play from start
+    audio.play();
+    key.classList.add('playing');
+    console.log(key);
+})
+
