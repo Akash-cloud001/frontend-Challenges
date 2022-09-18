f9fd3ded900c162b42efc8c40bbd2e5040f01524
Akash Parmar <ak.parmar1442000@gmail.com>
2022-09-18 13:53:34 +0530

    .

--- DrumSet/script.js
+++ DrumSet/script.js
@@ -7,11 +7,9 @@ function removeTransition(e){
 
 keys.forEach(key=>key.addEventListener('transitionend',removeTransition));
 
-
-
 window.addEventListener('keydown',(e)=>{
     const audio = document.querySelector(`audio[data-val="${e.keyCode}"]`);
-    const key = document.querySelector(`.keys[data-val="${e.keyCode}"]`);A
+    const key = document.querySelector(`.keys[data-val="${e.keyCode}"]`);
     if(!audio) return; // stops function running alltogether
     audio.currentTime = 0; // if we press audio again & again we want it to play from start
     audio.play();
